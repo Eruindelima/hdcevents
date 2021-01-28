4dfec22b8e6a5dcc9b39b88f0c57c1ac966007a2
Eruin de Lima Silva <48971348+Eruindelima@users.noreply.github.com>
2021-01-28 14:04:17 -0300

    criando a pagina de vizualição de eventos

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -32,16 +32,19 @@ class EventController extends Controller
 
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
             $requestImage = $request->image;
-
             $extension = $requestImage->extension();
-
             $imageName = md5($requestImage->getClientOriginalName().strtotime("now")).".".$extension;
-
             $requestImage->move(public_path('img/events'), $imageName);
-
             $event->image = $imageName;
         }
         $event->save();
         return redirect('/')->with('msg', 'Evento criado Com sucesso');
+    }
+
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
+
+        return view("events.show", ['event' => $event]);
     }
 }
