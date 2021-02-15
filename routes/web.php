0275f1b9e0b9559ce2fbbb840f82b76afc0c7c43
Eruin de Lima Silva <48971348+Eruindelima@users.noreply.github.com>
2021-02-15 14:31:01 -0300

    finalizando o projeto

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::put('/events/update/{id}', [EventController::class, 'update'])->middlewar
 Route::get('/contatos', [EventController::class, 'contact']);
 
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
+
+Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
+
+Route::delete('/events/leave/{id}', [EventController::class, 'leaveEvent'])->middleware('auth');
